b2921ce6528aa6de798d5794950bceb42583283b
DavidIsrael <davidisrael.rj@gmail.com>
2025-04-06 19:04:31 -0300

    drawer na tela de produtos

--- app/(tabs)/products/_layout.jsx
+++ app/(tabs)/products/_layout.jsx
@@ -1,20 +1,18 @@
-import { DrawerToggleButton } from "@react-navigation/drawer"
-import { Stack, useNavigation } from "expo-router";
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import { Stack } from "expo-router";
 
 export default function ProductsLayout() {
 
-    const navigation = useNavigation();
     return (
         <Stack
             screenOptions={{
-                headerStyle: { backgroundColor: "#e94560" },
-                headerTintColor: "#FFFF",
-                headerLeft: () => <DrawerToggleButton tintColor="#FFFFFF" />
-                
+                headerStyle: { backgroundColor: "#E94560" },
+                headerTintColor: "#FFFFFF",
+                headerRight: () => <DrawerToggleButton tintColor="#FFFFFF" />
             }}
         >
-            <Stack.Screen name="index"/>
-            <Stack.Screen name="products/[id]"/>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="products/[id]" />
         </Stack>
-    )
+    );
 }
